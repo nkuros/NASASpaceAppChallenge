5eeecb33dce8c7167ef401474de3d2da1028decc
nkurosawa <kuro.nori@hotmail.com>
2018-10-20 14:41:20 -0300

    BlobDetector now returns 10 biggest stars (whatever biggest means)

--- Python/BlobDetection/BlobDetector.py
+++ Python/BlobDetection/BlobDetector.py
@@ -7,10 +7,9 @@ image = cv2.imread("Hubble_Pics/01_veritable mix of different galaxies.jpg", cv2
 #Filter Noise
 im = cv2.medianBlur(image,9)
 
-#Invert image 
+#Invert image
 im= cv2.bitwise_not(im)
 
-#im = cv2.Sobel(im, cv2.CV_8U, 1, 1, ksize=3)
 
 # Set parameters for detection
 params = cv2.SimpleBlobDetector_Params()
@@ -23,21 +22,35 @@ params.maxThreshold = 255
 # params.filterByInertia = True
 # params.minInertiaRatio = 0.6
 
+# Set up minimum area threshold.
+params.filterByArea = True
+params.minArea = 1000
 
 # Set up the detector with default parameters.
 # detector = cv2.SimpleBlobDetector_create()
 
 # Set up the detector with parameters.
-detector = cv2.SimpleBlobDetector_create(params)
-
 # Detect blobs.
+detector = cv2.SimpleBlobDetector_create(params)
 keypoints = detector.detect(im)
-print(keypoints)
+
+starsize = []
+for elements in keypoints:
+    starsize.append(elements.size)
+starsize.sort(reverse=True)
+print(starsize)
+tenBiggest = []
+for i in range(10):
+    for j in range(len(keypoints)):
+        if starsize[i] == keypoints[j].size:
+            tenBiggest.append(keypoints[j])
+            break
 
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (0, 0, 255),
+im_with_keypoints = cv2.drawKeypoints(image, tenBiggest, np.array([]), (0, 0, 255),
                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
+#cv2.imwrite("img.jpg",im_with_keypoints)
 cv2.waitKey(0)
